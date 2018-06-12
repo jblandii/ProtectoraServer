7575339cdb4d8c838e07086bd4dcb14cef77b5ac
Jairo Ariza Martinez <jairo_ariza_95@hotmail.com>
2018-06-12 12:50:53 +0200

    Actualizado. Conversaciones

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -186,7 +186,7 @@ CKEDITOR_CONFIGS = {
 }
 
 # CONFIGURACIÓN DE CORREO
-DEFAULT_FROM_EMAIL = 'Protectora <protectora.jmgl@gmail.com>'
+DEFAULT_FROM_EMAIL = 'protectora.jmgl@gmail.com'
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
